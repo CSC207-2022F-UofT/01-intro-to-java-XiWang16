841f59670d3e4ab2e6fe9a3dba2d3d66c2eca0b6
XiWang16 <beholdxi.wang@mail.utoronto.ca>
2022-09-28 11:38:43 -0400

    completed all TODOs

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for(int count = 10; count >= 0; count--) {
+            System.out.println("Current count: " + count);
+        }
 
 
     }
@@ -143,6 +148,10 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] words = to_split.split(" ");
+        for (String word : words){
+            ret.append(word.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,6 +179,10 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        int i;
+        for (i = 1; i < arr.length; i += 2){
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
